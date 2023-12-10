dbe911ddcd50241959033f91489194afcdbf3da0
Jodmxx <108124413+Jodmxx@users.noreply.github.com>
2023-12-10 09:40:34 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -26,7 +26,7 @@ async def render_page(id, secure_hash):
             heading = 'Listen {}'.format(file_data.file_name)
             tag = file_data.mime_type.split('/')[0].strip()
             html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
-            mediaLink=f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}
+            mediaLink= online_link
     else:
         async with aiofiles.open('Adarsh/template/dl.html') as r:
             async with aiohttp.ClientSession() as s:
