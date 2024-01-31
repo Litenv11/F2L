8cb9cfddf180152665985e3a6a0cb681cb5803da
Jodmxx <108124413+Jodmxx@users.noreply.github.com>
2024-01-31 10:30:00 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -25,7 +25,7 @@ class Var(object):
     OWNER_USERNAME = str(getenv('OWNER_USERNAME' "mrx235"))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME', "zodf2l-ad57aa05d72d"))
+        APP_NAME = str(getenv('APP_NAME', "zodfl-1c24b11c7c22")
    
     else:
         ON_HEROKU = False
