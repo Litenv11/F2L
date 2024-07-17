f77c0e20f95e2a03e7cafeb9e9055b813432d22f
Litenv11 <fastx@chiccanvas.co.uk>
2024-07-17 20:42:03 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,7 +11,7 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv("API_ID", 29256711))
     API_HASH = str(getenv('API_HASH', "e1e7f435524bc0c756783949cad78fcd"))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', "6185171594:AAGvbbvFlejRYaWbiN_S6fsOvXbUOvTsnJ4"))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', "7284540699:AAECOM8zzDJPuevgtjokNFwAnAyaKMy4q8w"))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
@@ -19,10 +19,10 @@ class Var(object):
     PORT = int(getenv('PORT', ))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "2010387571").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5196029054").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME' "mrx235"))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME' "LitenV10"))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME', "fil2l-a2502557ebe7"))
